c19f031978e906a23d5149b2ee4f2a4a0e60b4a2
lennehendrickx <lennehendrickx@gmail.com>
2013-09-21 17:34:47 +0200

    created thumbnails directives + ie7 support

--- web/js/directives.js
+++ web/js/directives.js
@@ -3,4 +3,32 @@
 /* Directives */
 
 
-angular.module('faceit.directives', []);
+angular.module('faceit.directives', [])
+    .directive("thumbnails", function () {
+        return {
+            restrict: "EA",
+            transclude: true,     // it has HTML content
+            replace: true,        // replace the original markup with our template
+            scope: {},            // no scope variables required
+            template:             // template assigns class and transclusion element
+                "<ul class='thumbnails' ng-transclude></ul>",
+            controller: function () {}
+        };
+    })
+    .directive("thumbnail", function () {
+        return {
+            restrict: "EA",
+            transclude: true,     // it has HTML content
+            replace: true,        // replace the original markup with our template
+            require: "^thumbnails",
+            scope: {
+                link: "@",
+                imageUrl: "@"
+            },
+            template:             // template assigns class and transclusion element
+                '<li>\
+                    <a href="{{link}}" class="thumbnail" ng-transclude><img ng-src="{{imageUrl}}"></a>\
+                 </li>'
+        };
+    }
+);
